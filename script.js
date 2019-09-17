0cee9016c2e22132ad091fa4977d7328ab6765fd
Syaufy <46257169+Syauqizaidan@users.noreply.github.com>
2019-09-17 21:49:37 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+// {
+      //     var nama = (document.form.nama.value);
+      //     var tujuan = (document.form.tujuan.value);
+      //     var kelas = (document.form.kelas.value);
+      //     var jumlahtiket = parseFloat(document.form.tiket.value);
+      //     var hargatiket = 0.0;
+      //     var sub = 0.0;
+      //     var diskon =0.0;
+      //     var total =0.0;
+          
+      //     if ((tujuan=="Jakarta") && (kelas=="Eksekutif"))
+      //     {
+      //     hargatiket =70000;
+      //     }
+      //     else if ((tujuan=="Jakarta") && (kelas=="Bisnis"))
+      //     {
+      //     hargatiket =40000;
+      //     }
+      //     else if ((tujuan=="Jakarta") && (kelas=="Ekonomi"))
+      //     {
+      //       hargatiket =10000;
+      //     }
+      //     else if ((tujuan=="Solo") && (kelas=="Eksekutif"))
+      //     {
+      //       hargatiket =80000;
+      //     }
+      //     else if ((tujuan=="Solo") && (kelas=="Bisnis"))
+      //     {
+      //       hargatiket =50000;
+      //     }
+      //     else if ((tujuan=="Solo") && (kelas=="Ekonomi"))
+      //     {
+      //       hargatiket =20000;
+      //     }
+      //     else if ((tujuan=="Surabaya") && (kelas=="Eksekutif"))
+      //     {
+      //       hargatiket =90000;
+      //     }
+      //     else if ((tujuan=="Surabaya") && (kelas=="Bisnis"))
+      //     {
+      //       hargatiket =60000;
+      //     }
+      //     else
+      //     {
+      //       hargatiket =30000;
+      //     }
+      //     sub = jumlahtiket* hargatiket;
+      //     if (document.form.member.checked==true)
+      //     {
+      //     diskon =0.10*sub;
+      //     }
+      //     else
+      //     {
+      //     diskon=0.0;
+      //     }
+      //     total = sub-diskon;
+      //     document.form.hrgtiket.value=eval(hargatiket);
+      //     document.form.subtotal.value=eval(sub);
+      //     document.form.diskon.value=eval(diskon);
+      //     document.form.totalbayar.value=eval(total);
